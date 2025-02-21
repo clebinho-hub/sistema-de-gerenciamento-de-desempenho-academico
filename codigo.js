5849e203f4789599963d2c982b7d1944a95c44d4
clebinho-hub <clebinunesrebelo30@gmail.com>
2025-02-21 00:33:30 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,17 @@
+const botao = document.getElementById('botao');
+
+botao.addEventListener('click', function(event) {
+    event.preventDefault();  // Impede o formulário de ser enviado e a página de recarregar
+    
+    const nome = document.getElementById('username').value;  
+    const senha = document.getElementById('password').value;
+    
+    console.log("Botão clicado!");  // Verifica se o código está rodando
+    
+    if (nome === 'carlos' && senha === 'senha123') {
+        console.log("Dados corretos! Redirecionando...");
+        window.location.href = 'outraPagina.html';  // Redireciona para outra página
+    } else {
+        alert('Usuário ou senha não encontrados!');
+    }
+});
